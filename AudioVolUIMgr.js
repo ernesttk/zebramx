da2b6c5589c1afddc46490529a713f6382c3a24f
Ernest ter Kuile <ernest-ter.kuile@klm.com>
2025-08-31 16:33:39 +0200

    add more function to AudioVolMgr and test function

--- AudioVolUIMgr.js
+++ AudioVolUIMgr.js
@@ -42,19 +42,22 @@ var mx=importModule('zebraMX');
 
 const mgr = 'AudioVolUIMgr';
 
-exports.createSoundProfile = function createSoundProfile(profileName, {musicLvl=null, ringLvl=null, notifLvl=null, sysLvl=null, alarmLvl=null, callLvl=null, vvsLvl=null}) {
+exports.createProfile = function createSoundProfile(profileName, {musicLvl=null, ringLvl=null, notifLvl=null, sysLvl=null, alarmLvl=null, callLvl=null, vvsLvl=null}) {
     var params = '';
     if (musicLvl!=null)
         params += mx.buildParamXML("STREAM_MUSIC_SPK_LEVEL",musicLvl);
     if (ringLvl!=null)
         params += mx.buildParamXML("STREAM_RING_SPK_LEVEL",ringLvl);
     if (notifLvl!=null)
+        /* not supported but ignored above MX11 */
         params += mx.buildParamXML("STREAM_NOTIFICATION_SPK_LEVEL",notifLvl);
     if (sysLvl!=null)
+        /* not supported but ignored above MX11 */
         params += mx.buildParamXML("STREAM_SYSTEM_SPK_LEVEL",sysLvl);
     if (alarmLvl!=null)
         params += mx.buildParamXML("STREAM_ALARM_SPK_LEVEL",alarmLvl);
     if (callLvl!=null)
+        /* not supported but ignored above MX11 */
         params += mx.buildParamXML("STREAM_VOICECALL_SPK_LEVEL",callLvl);
     if (vvsLvl!=null)
         params += mx.buildParamXML("STREAM_VVS_SPK_LEVEL",vvsLvl);
@@ -81,8 +84,65 @@ exports.setCurrentProfile = function setCurrentProfile(profileName) {
     </characteristic>
   </characteristic>
 */
-    var command = mx.buildCharacteristicXML(mgr, mx.buildParamXML("CurrentProfileAction", "1") + mx.buildCharacteristicXML("CurrentUIProfile",
+    command = mx.buildCharacteristicXML(mgr, mx.buildParamXML("CurrentProfileAction", "1") + mx.buildCharacteristicXML("CurrentUIProfile",
         mx.buildParamXML("CurrentProfileName", profileName) + mx.buildParamXML("SetCurrentProfileOption", "2")));
+
+    var response = sendCommand(command);
+    if (response.hasOwnProperty("characteristic-error")) {
+        throw response["characteristic-error"].@desc;
+    }
+}
+
+exports.deleteProfile = function deleteProfile(profileName, resetToDefault=False) {
+/*  // remove old profiles
+  <characteristic version="10.2" type="AudioVolUIMgr">
+    <parm name="UIProfileAction" value="2" />
+    <characteristic type="UIProfile">
+      <parm name="ProfileName" value="Bagmanager_No_Scan_Beep" />
+    </characteristic>
+  </characteristic>
+*/
+    if (resetToDefault) {
+        exports.setFactoryDefaultProfile();
+    }
+
+    var command = mx.buildCharacteristicXML(mgr, mx.buildParamXML("UIProfileAction", "2") + mx.buildCharacteristicXML("UIProfile",
+        mx.buildParamXML("ProfileName", profileName)));
+
+    var response = sendCommand(command);
+    if (response.hasOwnProperty("characteristic-error")) {
+        throw response["characteristic-error"].@desc;
+    }
+}
+
+exports.setCurrentProfileSoundLevels = function setCurrentProfileSoundLevels() {
+/* set sound levels back to whatever was declared in the current profile */
+    command = mx.buildCharacteristicXML(mgr, mx.buildParamXML("setCurrentProfile", "2"));
+
+    var response = sendCommand(command);
+    if (response.hasOwnProperty("characteristic-error")) {
+        throw response["characteristic-error"].@desc;
+    }
+}
+
+exports.setFactoryDefaultProfile = function setFactoryDefaultProfile() {
+/* reset current profile to factory default */
+    command = mx.buildCharacteristicXML(mgr, mx.buildParamXML("setCurrentProfile", "3"));
+
+    var response = sendCommand(command);
+    if (response.hasOwnProperty("characteristic-error")) {
+        throw response["characteristic-error"].@desc;
+    }
+}
+
+exports.preventMute = function preventMute() {
+/*
+  <characteristic version="10.2" type="AudioVolUIMgr">
+    <parm name="MuteVibrateState" value="3" />
+  </characteristic>
+
+*/
+    var command = mx.buildCharacteristicXML(mgr, mx.buildParamXML("MuteVibrateState", "3"));
 
     var response = sendCommand(command);
     if (response.hasOwnProperty("characteristic-error")) {
--- test_AudioVolUIMgr.js
+++ test_AudioVolUIMgr.js
@@ -4,5 +4,8 @@ var audiomgr=importModule('AudioVolUIMgr');
 
 var profileName = "GroundServices";
 
-audiomgr.createSoundProfile(profileName, "6,15,12", "3,7,6", "5,7,6", "5,7,6", "5,7,6", "3,5,4", "7,10,8");
+audiomgr.createSoundProfile(profileName, {musicLvl:"6,15,12", ringLvl:"3,7,6", notifLvl:"5,7,6", sysLvl:"5,7,6", alarmLvl:"5,7,6", callLvl:"3,5,4", vvsLvl:"7,10,8"});
+audiomgr.setCurrentProfile(profileName);
+audiomgr.setCurrentProfileSoundLevels();
+audiomgr.preventMute();
 
