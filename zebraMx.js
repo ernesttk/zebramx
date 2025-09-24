861e3db5eb62833bece918945a84383fe3e55170
ernesttk <ernest.j.ter.kuile@gmail.com>
2025-09-24 10:01:40 +0200

    removed version test. was not appropriate here. I did add the option to add a version to the xml

--- zebraMx.js
+++ zebraMx.js
@@ -1,30 +1,8 @@
 #!/usr/bin/env js
 
+// set to true to get debug info in the log
 const debug = true;
-
 var mxVersion = null;
-var mxVersionMajor = 0;
-var mxVersionMinor = 0;
-
-function isMxVersionLarger(inputVersion) {
-	var equalOrLarger = false;
-	var inputMajor = 0;
-	var inputMinor = 0;
-	if (mxVersion == null)
-		exports.version();
-	var inputVersions = inputVersion.split(".");
-	if (inputVersions.length > 0)
-		inputMajor = parseInt(inputVersions[0]);
-	if (inputVersions.length > 1)
-		inputMinor = parseInt(inputVersions[1]);
-
-	if (mxVersionMajor > inputMajor)
-		equalOrLarger = true;
-	else if (mxVersionMajor == inputMajor && mxVersionMinor >= inputMinor)
-		equalOrLarger = true;
-
-	return equalOrLarger;
-}
 
 exports.version = function getMxVersion() {
 	if (mxVersion == null)
@@ -32,10 +10,6 @@ exports.version = function getMxVersion() {
 		var versionQuery = exports.buildCharacteristic("MX", '<parm-query name="Version" />');
 		var response = exports.sendCommand(versionQuery);
 		mxVersion = response..parm.@value;
-		var versions = mxVersion.split(".");
-		mxVersionMajor = parseInt(versions[0]);
-		if (versions.length > 1)
-			mxVersionMinor = parseInt(versions[1]);
 	}
 	return mxVersion;
 }
@@ -44,15 +18,34 @@ exports.buildParam = function buildParam(param, value) {
     return '<parm name="' + param + '" value="' + value + '" />';
 }
 
-exports.buildCharacteristic = function buildCharacteristic(type, enclosedXML) {
-    return '<characteristic type="' + type + '">' + enclosedXML + '</characteristic>';
+exports.buildCharacteristic = function buildCharacteristic(type, enclosedXML, mxInputVersion) {
+	var xmlversion = '';
+	var xmltype = ' type="' + type + '"';
+	if (mxInputVersion !== undefined)
+	    xmlversion = ' version="'+ mxInputVersion +'"'
+
+	mobicontrol.log.info("xmlversion: " + xmlversion);
+	mobicontrol.log.info("xmltype: " + xmltype);
+
+	return '<characteristic' + xmlversion + xmltype + '>' + enclosedXML + '</characteristic>';
 }
 
-exports.sendCommand = function sendCommand(command, debug = false) {
+// Send the command to the MX handler.
+exports.sendCommand = function sendCommand(command) {
     if (!command.startsWith('<wap-provisioningdoc>')) {
         command = '<wap-provisioningdoc>' + command + '</wap-provisioningdoc>';
     }
     if (debug)
 		  mobicontrol.log.info("command: " + command);
-    return new XML(mobicontrol.mdm.configure(command));
+    
+	var response = new XML(mobicontrol.mdm.configure(command));
+	if (response.hasOwnProperty("characteristic-error")) {
+	    if (debug)
+		  	mobicontrol.log.error("response: " + response.toString());
+
+		throw response["characteristic-error"].@desc;
+	}
+	
+	// return the XML. 
+	return response;
 }
