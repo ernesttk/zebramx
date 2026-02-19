5350a8f659bd75aa9e50d7f856daf7924eb6a035
ernesttk <ernest.j.ter.kuile@gmail.com>
2026-02-19 07:52:56 +0100

    =corrected some of the MX parameters using the wrong case for the command. Also added all the force orientation helper functions

--- displayMgr.js
+++ displayMgr.js
@@ -9,6 +9,12 @@ const enable = "1";
 const disable = "2";
 var debug = false;
 
+// set to true to get debug info in the log
+exports.setExtraLog = function (value = true) {
+    mx.setExtraLog (value);
+    debug = value;
+}
+
 exports.setDisplayTimeout = function (timeoutSeconds) {
     const useMX = "4.3";
     // common values is 300 -> 5 minutes
@@ -30,7 +36,7 @@ exports.setBrightnessLevel = function (level) {
 
 exports.enableAutoBrightness = function () {
     const useMX = "10.2";
-    var command = mx.buildCharacteristic(mgr, mx.buildParam("AdaptiveBrightness", "1"), useMX);
+    var command = mx.buildCharacteristic(mgr, mx.buildParam("Adaptivebrightness", "1"), useMX);
 
     var response = mx.sendCommand(command);
     if (debug)
@@ -38,23 +44,48 @@ exports.enableAutoBrightness = function () {
 }
 
 exports.enableAutorotate = function () {
-    setAutorotate("Turn On");
+    setAutorotate("1");
 }
 exports.disableAutorotate = function () {
-    setAutorotate("Turn Off");
+    setAutorotate("2");
 }
 
 function setAutorotate(value) {
     const useMX = "10.1";
-    var command = mx.buildCharacteristic(mgr, mx.buildParam("AutoRotate", value), useMX);
+    var command = mx.buildCharacteristic(mgr, mx.buildParam("Autorotate", value), useMX);
 
     var response = mx.sendCommand(command);
     if (debug)
         mobicontrol.log.debug("Response : " + response.toString());
 }
 
-exports.stayAwakeOnPower = function () {
-    stayAwake(enable);
+exports.forceToPortrait = function () {
+    lockedOrientation("0");
+}
+
+exports.forceToLandscapeLeft = function () {
+    lockedOrientation("1");
+}
+
+exports.forceToPortraitInverted = function () {
+    lockedOrientation("2");
+}
+
+exports.forceToLandscapeRight = function () {
+    lockedOrientation("3");
+}
+
+
+function lockedOrientation(orientation) {
+    const useMX = "10.5";
+
+    exports.disableAutorotate ();
+
+    var command = mx.buildCharacteristic(mgr, mx.buildParam("LockedOrientation", orientation), useMX);
+
+    var response = mx.sendCommand(command);
+    if (debug)
+        mobicontrol.log.debug("Response : " + response.toString());
 }
 
 exports.allowSleepOnPower = function () {
