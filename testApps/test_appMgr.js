9762154ea4be10232ec292086672226cbb214303
Ernest ter Kuile <ernest-ter.kuile@klm.com>
2026-06-21 21:41:24 +0200

    added tests

--- testApps/test_appMgr.js
+++ testApps/test_appMgr.js
@@ -3,6 +3,18 @@
 
 var appMgr = require('js/appMgr/appMgr.js');
 
+appMgr.setExtraLog(true);
 
 appMgr.disableApplication('com.android.chrome');
 appMgr.enableApplication('com.android.chrome');
+
+appMgr.clearApplicationCache('com.microsoft.teams');
+
+require('js/appMgr/appMgr.js').clearApplicationCache('com.microsoft.teams');
+
+require('js/appMgr/appMgr.js').clearApplicationUserData('com.microsoft.teams');
+
+
+require('js/appMgr/appMgr.js').setDefaultLauncher('com.android.launcher3');
+
+require('js/appMgr/appMgr.js').setDefaultLauncher('com.sita.browser');
